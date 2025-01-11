747f20d698944bc9227a4b351a99d1b10f4c1e72
4stra <78592383+mosahosseini@users.noreply.github.com>
2025-01-11 23:26:04 +0100

    Deletion and insertion done
    //Done:
    1. Fixed so that insert uses h in struct
    2. Deleted balance argument in struct.
    //TODOO:
    1. Clean the code
    2. rename get_balancee to get_balance

--- Lesson11-Algorithms/Lists_Binary_Trees_Grapths/Avl_tree.c
+++ Lesson11-Algorithms/Lists_Binary_Trees_Grapths/Avl_tree.c
@@ -3,7 +3,6 @@
 
 typedef struct Node {
 int key; 
-int balance; // range of (-1 to 1) and it is height of 
 int h;
 struct Node * left ;
 struct Node * right; 
@@ -17,7 +16,6 @@ node *  init_tree(int key){
         exit(1);
     }
     root -> key = key ; 
-    root ->balance = 0 ; 
     root -> h  =1; 
     root -> left = NULL; 
     root -> right = NULL;
@@ -67,8 +65,8 @@ int get_balance(node* root ){
     return right - left;
 }
 
-int insert(node** root , int key   ){
-    int h = 0 ; 
+void  insert(node** root , int key   ){
+//    int h = 0 ; 
     if ((*root) == NULL){
         (*root) = (node*) malloc(sizeof(node));
         if((*root)== NULL){
@@ -79,99 +77,64 @@ int insert(node** root , int key   ){
         (*root) -> key = key; 
         (*root) -> left = NULL;
         (*root) -> right = NULL; 
-        (*root) -> balance = 0;
         (*root) -> h = 1;
-        return 1; 
+//        return 1; 
     }
     else{
 
     if ( key < (*root) ->key ){
-        h = insert( & (*root)->left , key  );
+//        h = insert( & (*root)->left , key  );
+        insert( & (*root)->left , key  );
 
 
-        if (h == 1){
+//        if ((*root)->h >2){
             (*root) -> h = max(height( (*root) -> left ) , height( (*root) -> right )) +1 ; 
-
-            if ((*root) -> balance == 1){
-                (*root)-> balance =0;
-                h = 0;
+            int balance = get_balancee((*root));
+            int balance2 = get_balancee( (*root) -> left );
+            
+            //left heavy ->  rotate to right 
+            if ( balance > 1 && get_balancee((*root) -> left) >=0 ){
+                // single right rotation
+                node * s = (*root) -> left; 
+                node * t = s-> right; 
+                (*root)->left = t; // possible error; 
+                (*root) -> h = max(height( (*root) -> left ) , height((*root) -> right )) +1 ; 
+                s->right = (*root);
+//                (*root)->balance = 0;
+                (*root) = s;
+                s -> h = max(height( s -> left ) , height( s -> right )) +1 ;
             }
+            else if ( balance > 1 && get_balancee((*root) -> left) <0 )  {
+                //Double right rot
+                node * s = (*root) -> left; 
+                node * t = (*root) -> left -> right;
+                s-> right = t-> left ;
+                s -> h = max(height( s -> left ) , height( s -> right )) +1 ;
 
-            else if ((*root) ->balance == 0){
-                (*root)->balance = -1;
-            }
-            else{
-                if ((*root) -> left -> balance == -1){
-                    // single rigth rotation
-                    node * s = (*root) -> left; 
-                    node * t = s-> right; 
-                    (*root)->left = t; // possible error; 
-                    (*root) -> h = max(height( (*root) -> left ) , height((*root) -> right )) +1 ; 
-                    s->right = (*root);
-                    (*root)->balance = 0;
-                    (*root) = s;
-                    s -> h = max(height( s -> left ) , height( s -> right )) +1 ;
-                }
-                else {
-                    //Double right rot
-                    node * s = (*root) -> left; 
-                    node * t = (*root) -> left -> right;
+                t->left   = s; 
 
+                (*root) -> left = t-> right;
+                (*root) -> h = max(height( (*root) -> left ) , height((*root) -> right )) +1 ; 
 
-                    s-> right = t-> left ;
-                    s -> h = max(height( s -> left ) , height( s -> right )) +1 ;
+                t-> right = (*root);
 
-                    t->left   = s; 
-
-                    (*root) -> left = t-> right;
-                    (*root) -> h = max(height( (*root) -> left ) , height((*root) -> right )) +1 ; 
-
-                    t-> right = (*root);
-
-
-                    if (t->balance == -1){
-                        t->right->balance = 1;
-                    }
-                    else{
-                        t->right -> balance = 0 ; 
-                    }
-
-                    if (t->balance == 1){
-                        s->balance = -1;
-                    }
-                    else{
-                        s->balance = 0;
-                    }
-
-                    t -> h = max(height( t -> left ) , height(t -> right )) +1 ;                     
-                    (*root)= t; 
-                }
-                
-                (*root)->balance = 0;
-                h=0;
-            }
+                t -> h = max(height( t -> left ) , height(t -> right )) +1 ;                     
+                (*root)= t; 
+//            }
+            
 
 
         }
 //        (*root) -> balance = get_balance((*root));
     } 
     else if ( key > (*root) ->key  ) {
-        h = insert(&(*root) -> right , key);
-        if (h == 1){
+        //h = insert(&(*root) -> right , key);
+        insert(&(*root) -> right , key);
             (*root) -> h = max(height( (*root) -> left ) , height((*root) -> right )) +1 ; 
-            
-            if ((*root)->balance == -1){
-            (*root) -> balance = 0 ; 
-  
-            h = 0 ; 
-            }
 
-            if ((*root) -> balance == 0 ){
-                (*root) -> balance = 1; 
-            }
-
-            else{
-                if((*root) ->right -> balance == 1){
+            int balance = get_balancee((*root));            
+                //right heavy
+                if( balance < -1 && get_balancee((*root) -> right) <= 0){
                     //single left rotation. 
                     node *s = (*root) -> right;
                     (*root) -> right  = s->left;
@@ -179,10 +142,9 @@ int insert(node** root , int key   ){
 
                     s->left = (*root);
                     s -> h = max(height( s -> left ) , height(s -> right )) +1 ; 
-                    (*root) -> balance = 0 ; 
                     (*root) = s; 
                 }
-                else{
+                else if ( balance < -1 && get_balancee((*root) -> right) > 0) {
                     // double left rotation
                     node * s = (*root) -> right;
                     node * t = s->left ; 
@@ -193,32 +155,17 @@ int insert(node** root , int key   ){
                     (*root) -> h = max(height( (*root) -> left ) , height((*root) -> right )) +1 ;  
                     t->left = (*root) ;
                     t -> h = max(height( t -> left ) , height(t -> right )) +1 ; 
-                    if (t->balance == 1){
-                        t->left -> balance = -1;
-                    }
-                    else {
-                        t->left -> balance = 0;
-                    }
-
-                    if(t-> balance == -1){
-                        s-> balance = 1;
-                    }
-                    else {
-                        s->balance = 0; 
-                    }
     
                     (*root) = t ;
 
                 }
-                (*root) -> balance = 0 ; 
-                h = 0 ;
-
-            }    
+//
+//                h = 0 ;    
         }
 //        (*root) -> balance = get_balance((*root));
     }
-    }
-    return h ;  
+
+//    return h ;  
     
  }
 
@@ -307,6 +254,7 @@ node* delete(node** root , int key ){
 if ((*root) == NULL){
     return (*root); 
 }
+
 else if( key < (*root)->key){
     (*root) -> left = delete(&((*root)-> left) , key);
 }
@@ -356,6 +304,7 @@ else {
 //     If balance > 1 AND GetBalance(root.left) >= 0:
 //         Return RightRotate(root)
 
+
     if (balance > 1 && get_balancee( (*root) -> left ) >= 0){
         return right_rotate((*root));
     }
@@ -365,7 +314,7 @@ else {
 //         root.left = LeftRotate(root.left)
 //         Return RightRotate(root)
 
-    if ( balance >1 && get_balancee( (*root) -> left ) >= 0  ){
+    if ( balance >1 && get_balancee( (*root) -> left ) < 0  ){
         (*root) -> left = left_rotate( (*root) -> left);
         return right_rotate( (*root)); 
     }
@@ -386,58 +335,11 @@ else {
 
 //     Return root
 
-
     if ( balance < -1  && get_balancee( (*root) -> right) > 0 ){
         (*root) -> right = right_rotate( (*root) -> right );
         return left_rotate( (*root) ); 
     }
 
-
-
-
-// Function RightRotate(y):
-//     x = y.left
-//     T2 = x.right
-    
-//     // Perform rotation
-//     x.right = y
-//     y.left = T2
-
-//     // Update heights
-//     y.height = Max(Height(y.left), Height(y.right)) + 1
-//     x.height = Max(Height(x.left), Height(x.right)) + 1
-    
-//     Return x
-
-// Function LeftRotate(x):
-//     y = x.right
-//     T2 = y.left
-    
-//     // Perform rotation
-//     y.left = x
-//     x.right = T2
-    
-//     // Update heights
-//     x.height = Max(Height(x.left), Height(x.right)) + 1
-//     y.height = Max(Height(y.left), Height(y.right)) + 1
-    
-//     Return y
-
-// Function GetBalance(node):
-//     If node is NULL:
-//         Return 0
-//     Return Height(node.left) - Height(node.right)
-
-// Function Height(node):
-//     If node is NULL:
-//         Return 0
-//     Return node.height
-
-// Function FindMin(node):
-//     current = node
-//     While current.left is not NULL:
-//         current = current.left
-//     Return current
 
 
 return (*root);
@@ -461,7 +363,7 @@ void print_tree(node * root , int level){
     }
 
     ++level; 
-    printf("Level: %d     value:%d    Balance:%d \n" , level , root -> key , root-> balance);
+    printf("Level: %d     value:%d    Balance:%d \n" , level , root -> key , get_balancee(root));
 
     print_tree(root->left , level);
     print_tree(root->right , level);
@@ -477,7 +379,7 @@ int main(){
     int k [6] = {20,10,25,30,22,50};
     
     for( int i = 0 ; i<6 ; i++){
-        int h = insert(&root , k[i]);
+        insert(&root , k[i]);
     }
 
     node * deleted_node =  delete( &root , 25);
