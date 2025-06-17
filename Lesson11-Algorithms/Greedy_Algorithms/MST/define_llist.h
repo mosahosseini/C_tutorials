9297da4a1b3718be7a1d51b56ade1f134f15482f
4stra <78592383+mosahosseini@users.noreply.github.com>
2025-06-18 01:45:44 +0200

    still trying to comment the code for prims algorithm. done: * documentation for heap.h documentation for prims algorithm
    //TODOO:
    comment: generic linked list.h

--- Lesson11-Algorithms/Greedy_Algorithms/MST/define_llist.h
+++ Lesson11-Algorithms/Greedy_Algorithms/MST/define_llist.h
@@ -2,6 +2,13 @@
 #include <stdio.h>
 #include <string.h>
 
+
+/*
+This is  the struct defining the edge. An edge is defined by: 
+* Index of the destination node.
+* The weight between those two.
+* A pointer to the next node. 
+*/
 typedef struct edge {
     int dest ;
     double weight;  
@@ -9,21 +16,69 @@ typedef struct edge {
 
 }edge; 
 
+
+/**
+ * Function: get_next
+ * ------------------------------
+ * This function will return the pointer to the next node.
+ *
+ * Parameters:
+ *   - node: A pointer to the current node.
+ *
+ * Behavior:
+ *   - Calls the next on the current node.
+ *
+*/
 void* get_next(void * node){
     return ((edge*)node) ->next;
 }
 
+
+/**
+ * Function: set_next
+ * ------------------------------
+ * This function will return set the next node..
+ *
+ * Parameters:
+ *   - node_1: A pointer to the current node.
+ *   - node_2: A pointer to the node which we want to insert.
+ *
+*/
 void set_next(void *node_1 , void* node_2 ){
     ((edge*)node_1)->next = (edge*)node_2;
 }
 
+/**
+ * Function: compare
+ * ------------------------------
+ * This function will compare the wheights of the two nodes. 
+ *
+ * Parameters:
+ *   - a: A pointer to the first node.
+ *   - b: A pointer to the second node
+ * 
+ * Behavior:
+ *   - Because the weight can be very large , we cant simply use a -> weight - b -> weight. We must use a logical operator. 
+ *
+*/
 int compare(const void* a , const void* b){
     const edge * x = (const edge *)a ; 
     const int * key = (int *)b;
     return (x->weight > *key) - (x->weight < *key);;
 }
 
-
+/**
+ * Function: print_node
+ * ------------------------------
+ * This function prints a node and its all nodes.
+ *
+ * Parameters:
+ *   - node: A pointer to a node.
+ *
+ * Behavior:
+ *   - Prints nodes destination index and its weight.
+ *
+*/
 void print_node(void* n){
     edge* node = (edge*)n;
     printf("dest: %d  , weight:%f \n" , node ->dest ,node ->weight);
--- Lesson11-Algorithms/Greedy_Algorithms/MST/prims_algorithm.c
+++ Lesson11-Algorithms/Greedy_Algorithms/MST/prims_algorithm.c
@@ -50,58 +50,62 @@ void create_heap(heap_t *h ,item_t * items , size_t n , size_t start_node_ind )
 
     // Initialize heap with array
     init_heap(h, &items[1], sizeof(item_t));
-    // printf("Initial heap state:\n");
-    // for (size_t i = 1; i <= heap_size(h); i++) {
-    //     printf("  a[%zu] = %d\n", i, ((item_t *)h->a[i])->ind);
-    // }
 
 }
 
 
 
 int prims_algorithm( list_t ** graph , heap_t * h , item_t* items , size_t n ){
-    int sum = 0 ; 
+    int sum = 0 ; // this variable will keep track of the lowest weights.
 
-    item_t * current = (item_t *) heap_min(h);
+    item_t * current = (item_t *) heap_min(h); // retrieve the node with minimum cost. (because we sett all other nodes except the start node to infinity, this will be our start node.)
     
     while (current != NULL){
-        current->visited = 1;
-        sum += current -> cost;
-        int curr_ind = current -> ind ; 
-        edge * temp = (edge *) (graph[curr_ind] -> head);
+        current->visited = 1; // We change the node to visited. 
+        sum += current -> cost; // include the edge.
+        int curr_ind = current -> ind ; // the index of current node in the graph.
+        edge * temp = (edge *) (graph[curr_ind] -> head); //we retrieve a head of the current index.
 
-        while (temp != NULL){
+        while (temp != NULL){  //traverse through all nodes. 
 
-            int dest_ind = temp ->dest ; 
-            
+            int dest_ind = temp ->dest ; // get the destination nodes index.
+            /*
+                If the destination node is not visited and the weight of current node is lower that the cost of destination node.curr_ind
+                set the cost to the weight of the current. 
+                change position in the heap. 
+            */
             if (!items[dest_ind].visited  && temp->weight < items[dest_ind].cost) {
             items[dest_ind].cost = temp->weight;
             heap_change_position(h, &items[dest_ind]);
             }
-            // items[dest_ind].cost = temp ->weight;
-            // item_t * w  = &items[dest_ind];
-            // heap_change_position(h, &items[dest_ind]);
+
+            //get the next node in the linked list. 
             temp =  get_next(temp);
             
         }
+        // now we retrieve the node with min cost.
         current = (item_t *) heap_min(h);
 
 
     }
+
+    // returning the sum of lowest weights.
     return sum;
 }
 
 
 
 int main(){
-    int MAX_LENGTH = 256;  
-    int n; 
-    int m; 
-    char * buffer = (char*)malloc(MAX_LENGTH); 
-    // FILE * file = fopen("input.txt" , "r");
+    int MAX_LENGTH = 256;  //buffer size
+    int n;                 // variable n number of nodes
+    int m;                 // variable m number of node for which we calculated the weights. 
+
+    char * buffer = (char*)malloc(MAX_LENGTH); // allocate memory for buffer.
     
-    if(fgets(buffer, MAX_LENGTH , stdin ) != NULL){
-        sscanf(buffer , "%d %d", &n , &m);
+    
+    // FILE * file = fopen("input.txt" , "r"); uncomment this if you want to run an specific file.
+    if(fgets(buffer, MAX_LENGTH , stdin ) != NULL){   // change the stdin to file if you run it for specific file.
+        sscanf(buffer , "%d %d", &n , &m); // sscan for setting values for n and m. 
     }
     else{
         printf("No input provided!");
@@ -109,15 +113,23 @@ int main(){
         return -1;
     }
 
+    /*
+    graph representation: For graph we use a adjecency list representation.
+    the graph is an array which contains pointers to linked list. 
+    */
+    // allocating memory for the graph. The heap is using indecies 1-n so we need to allocate n+1. 
     list_t ** graph = (list_t **)malloc(sizeof(list_t*) * (n+1));
+
     if (graph ==NULL){
             fprintf(stderr, "\nError: Graph initiation failed! \n");
             exit(EXIT_FAILURE);
         
     }
+
+    // first element of the array is not used therefore we set it no NULL
     graph[0] = NULL;
     for(int i = 1 ; i <=n ; i++ ){
-        graph[i] = new_list(compare , get_next, set_next );
+        graph[i] = new_list(compare , get_next, set_next ); // We initiate each graph element.
     }
     
     int ind, dest;
@@ -125,28 +137,29 @@ int main(){
     for (int j = 0 ; j < m ; j++ ){
         if(fgets(buffer , MAX_LENGTH , stdin) != NULL){
             sscanf(buffer , "%d %d %lf" ,&ind , &dest , &weight);
-            edge * node = malloc(sizeof(edge));
+            edge * node = malloc(sizeof(edge)); // allocating memory for an ede structure.
+            // set the dest weight parameters of this node. 
             node -> dest = dest ; 
             node -> weight = weight;
+            list_insert_front(graph[ind] , node ); 
+            // because the graph is undirected , we need to add add the edge to the destination node too. 
+            // so there is an edge from ind  to dest and there is another edge with the same weight from dest to ind. 
             edge * node2 = malloc(sizeof(edge));
             node2 -> dest = ind;
             node2 -> weight = weight ; 
-            list_insert_front(graph[ind] , node );
-            list_insert_front(graph[dest] , node2);
+            list_insert_front(graph[dest] , node2); 
         }
     }
 
-    // for (int i = 1 ; i <n ; i++){
-    //     printf("source: %d  ," , i);
-    //     list_foreach( graph[i], &print_node);
-    // }
-
+    //allocate memory for the heap array.
     item_t * items = malloc(sizeof(item_t)*(n+1));
-    heap_t *h = new_heap(n, compare_items, get_position);
-    create_heap(h , items, n , 1 );
-    int  c = prims_algorithm(graph , h , items , n);
+    heap_t *h = new_heap(n, compare_items, get_position); // we initiated a new heap. 
+    create_heap(h , items, n , 1 );  // put the elements in te heap. 
+    int  c = prims_algorithm(graph , h , items , n); 
 
     printf("%d\n" , c );
+
+
     // Free the edges inside the graph
     for (int i = 1; i <= n; i++) {
         edge *temp = (edge *) graph[i]->head;
@@ -165,7 +178,7 @@ int main(){
     free(h);
     free(items);
     free(buffer);
-    // fclose(file);
+    // fclose(file);   //uncomment this if you use specific files. 
     
     return 0;
 }
